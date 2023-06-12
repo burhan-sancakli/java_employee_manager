58068e2027b3e308737e460deec68df1b29424d3
brand02 <burhan.sancakli@gmail.com>
2023-06-12 23:01:40 +0200

    added more functionality, tidied the code up

--- Employee.java
+++ Employee.java
@@ -1,14 +1,13 @@
-import java.lang.InterruptedException;
 import java.util.concurrent.TimeUnit;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.text.SimpleDateFormat;
 import java.text.ParseException;
-public class Employee
-{
+
+public class Employee {
     final static String FILE_DEST = "vhodnipodatki2.txt";
-    
+
     public int id;
     public String surname;
     public String name;
@@ -22,7 +21,10 @@ public class Employee
     public int job_year;
 
     public double value_of_work_done;
-    public Employee(int id, String surname, String name, int number_of_hours, double hourly_rate, int birth_day, int birth_month, int birth_year, int job_day, int job_month, int job_year){
+
+    public Employee(int id, String surname, String name, int number_of_hours, double hourly_rate, int birth_day,
+            int birth_month, int birth_year, int job_day, int job_month, int job_year) {
+        this.id = id;
         this.surname = surname;
         this.name = name;
         this.number_of_hours = number_of_hours;
@@ -33,98 +35,166 @@ public class Employee
         this.job_day = job_day;
         this.job_month = job_month;
         this.job_year = job_year;
-        
+
         this.value_of_work_done = this.calculateValueOfWorkDone();
     }
-    private double calculateValueOfWorkDone(){
-        long days_of_work = calculateDateDifference(this.job_day, this.job_month, this.job_year);
-        return Double.valueOf(days_of_work*this.number_of_hours*this.hourly_rate);
-    }
-    private int getAge(){
-        return (int) calculateDateDifference(this.birth_day,this.birth_month,this.birth_year);
+
+    private double calculateValueOfWorkDone() {
+        long days_of_work = this.getServiceLength();
+        return Double.valueOf(days_of_work * this.number_of_hours * this.hourly_rate);
     }
 
-     public static long calculateDateDifference(int job_day, int job_month, int job_year) {
+    private int getAge() {
+        return (int) calculateDateDifference(this.birth_day, this.birth_month, this.birth_year);
+    }
+
+    private long getServiceLength() {
+        long days_of_work = calculateDateDifference(this.job_day, this.job_month, this.job_year);
+        return days_of_work;
+    }
+
+    public static long calculateDateDifference(int job_day, int job_month, int job_year) {
         try {
             SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-            Date jobDate = sdf.parse(String.format("%04d-%02d-%02d",job_year,job_month,job_day));
+            Date jobDate = sdf.parse(String.format("%04d-%02d-%02d", job_year, job_month, job_day));
             Date currentDate = sdf.parse(sdf.format(new Date()));
             long diff = currentDate.getTime() - jobDate.getTime();
             return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
-        }
-        catch (ParseException e)
-        {
+        } catch (ParseException e) {
             return -1;
         }
     }
-    private static ArrayList<Employee> getAllEmployees(String fileDest){
-        try
-        {
+
+    private static ArrayList<Employee> getAllEmployees(String fileDest) {
+        try {
             ArrayList<Employee> employees = new ArrayList<>();
             Scanner sc = new Scanner(new File(fileDest), "utf-8");
-            //parsing a CSV file into the constructor of Scanner class 
-            sc.nextLine();
+            // parsing a CSV file into the constructor of Scanner class
+            sc.nextLine(); // skip the columns row.
             while (sc.hasNextLine()) {
                 String[] line = sc.nextLine().split(" ");
                 Employee employee = new Employee(
-                    Integer.parseInt(line[0]),
-                    line[1],
-                    line[2],
-                    Integer.parseInt(line[3]),
-                    Double.parseDouble(line[4]),
-                    Integer.parseInt(line[5]),
-                    Integer.parseInt(line[6]),
-                    Integer.parseInt(line[7]),
-                    Integer.parseInt(line[8]),
-                    Integer.parseInt(line[9]),
-                    Integer.parseInt(line[10])
-                );
+                        Integer.parseInt(line[0]),
+                        line[1],
+                        line[2],
+                        Integer.parseInt(line[3]),
+                        Double.parseDouble(line[4]),
+                        Integer.parseInt(line[5]),
+                        Integer.parseInt(line[6]),
+                        Integer.parseInt(line[7]),
+                        Integer.parseInt(line[8]),
+                        Integer.parseInt(line[9]),
+                        Integer.parseInt(line[10]));
                 employees.add(employee);
             }
             sc.close();
-            //closes the scanner 
+            // closes the scanner
             return employees;
-        }
-        catch (FileNotFoundException ex)  
-        {
+        } catch (FileNotFoundException ex) {
             return new ArrayList<Employee>();
         }
     }
+
+    private static ArrayList<Employee> getAllEmployeesWithIds(String fileDest, int[] ids) {
+        try {
+            ArrayList<Employee> employees = new ArrayList<>();
+            Scanner sc = new Scanner(new File(fileDest), "utf-8");
+            // parsing a CSV file into the constructor of Scanner class
+            sc.nextLine(); // skip the columns row.
+            while (sc.hasNextLine()) {
+                String[] line = sc.nextLine().split(" ");
+                if (!contains(ids, Integer.parseInt(line[0]))) {
+                    continue;
+                }
+                Employee employee = new Employee(
+                        Integer.parseInt(line[0]),
+                        line[1],
+                        line[2],
+                        Integer.parseInt(line[3]),
+                        Double.parseDouble(line[4]),
+                        Integer.parseInt(line[5]),
+                        Integer.parseInt(line[6]),
+                        Integer.parseInt(line[7]),
+                        Integer.parseInt(line[8]),
+                        Integer.parseInt(line[9]),
+                        Integer.parseInt(line[10]));
+                employees.add(employee);
+            }
+            sc.close();
+            // closes the scanner
+            return employees;
+        } catch (FileNotFoundException ex) {
+            return new ArrayList<Employee>();
+        }
+    }
+
     private static ArrayList<Employee> sortBySurname(ArrayList<Employee> employees) {
         Collections.sort(employees, new Comparator<Employee>() {
             @Override
-            public int compare(Employee e1, Employee e2){
-            return e1.surname.compareTo(e2.surname);
-            }  
+            public int compare(Employee e1, Employee e2) {
+                return e1.surname.compareTo(e2.surname);
+            }
         });
         return employees;
     }
-    private static String toJson(ArrayList<Employee> employees){
+
+    private static String toJson(ArrayList<Employee> employees) {
         String json = "[";
         boolean isFirst = true;
-        for (Employee employee : employees) 
-        {
-            if(!isFirst) { json += ","; }
+        for (Employee employee : employees) {
+            if (!isFirst) {
+                json += ",";
+            }
             isFirst = false;
-            json +="{\"id\":"+employee.id+", \"surname\":\""+employee.surname+"\", \"name\":\""+employee.name+"\", \"number_of_hours\":"+employee.number_of_hours+", \"hourly_rate\":\""+employee.hourly_rate+"\", \"birth_day\":"+employee.birth_day+", \"birth_month\":"+employee.birth_month+", \"birth_year\":"+employee.birth_year+", \"job_day\":"+employee.job_day+", \"job_month\":"+employee.job_month+", \"job_year\":"+employee.job_year+", \"value_of_work_done\":\""+employee.value_of_work_done+"\"}";
+            json += "{\"id\": " + employee.id + ", \"surname\":\"" + employee.surname + "\", \"name\":\""
+                    + employee.name
+                    + "\", \"number_of_hours\":" + employee.number_of_hours + ", \"hourly_rate\":\""
+                    + employee.hourly_rate + "\", \"birth_day\":" + employee.birth_day + ", \"birth_month\":"
+                    + employee.birth_month + ", \"birth_year\":" + employee.birth_year + ", \"job_day\":"
+                    + employee.job_day + ", \"job_month\":" + employee.job_month + ", \"job_year\":" + employee.job_year
+                    + ", \"value_of_work_done\":\"" + employee.value_of_work_done + "\"}";
         }
         json += "]";
         return json;
     }
 
-    public static void main(String[] args)
-    {
+    private static String getEmployeesAgeAndServiceLengthJson(ArrayList<Employee> employees, int[] ids) {
+        String json = "[";
+        boolean isFirst = true;
+        for (Employee employee : employees) {
+            if (!isFirst) {
+                json += ",";
+            }
+            isFirst = false;
+            json += "{\"id\": " + employee.id + ", \"age\": " + employee.getAge() + ", \"service_length\": "
+                    + employee.getServiceLength() + " }";
+        }
+        json += "]";
+        return json;
+    }
+
+    public static void main(String[] args) {
         String response = "";
         String API_ENDPOINT = args[0];
         switch (API_ENDPOINT) {
+
             case "get-all-employees":
                 response = toJson(getAllEmployees(FILE_DEST));
                 break;
-    
+
             case "get-all-employees-sorted-by-surname":
                 ArrayList<Employee> employees = getAllEmployees(FILE_DEST);
                 employees = sortBySurname(employees);
                 response = toJson(employees);
+                break;
+
+            case "get-employees-age-and-service":
+                int[] ids = new int[args.length - 1];
+                for (int i = 1; i < args.length; i++) {
+                    ids[i - 1] = Integer.parseInt(args[i]);
+                }
+                ArrayList<Employee> limitedEmployees = getAllEmployeesWithIds(FILE_DEST, ids);
+                response = getEmployeesAgeAndServiceLengthJson(limitedEmployees, ids);
                 break;
 
             // Default case
@@ -135,5 +205,9 @@ public class Employee
         }
         String encodeString = Base64.getEncoder().encodeToString(response.getBytes(StandardCharsets.UTF_8));
         System.out.print(encodeString);
+    }
+
+    private static boolean contains(final int[] arr, final int key) {
+        return Arrays.stream(arr).anyMatch(i -> i == key);
     }
 }
--- index.js
+++ index.js
@@ -9,37 +9,78 @@ const { JavaCaller } = require("java-caller");
 
 
 var http = require("http");
+const fs = require('fs');
+const url = require('url');
 
 console.log("Starting the web server, go to: http://127.0.0.1:8080/ to test.");
 
-http.createServer(async function(req, res){
-    const employees = await getAllEmployees();
-    console.log(employees);
-    res.writeHead(200, {"Content-Type": "text/html"});
-    res.end(`
-    <!DOCTYPE html>
-    <html lang="sl">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Employee Manager</title>
-    </head>
-    <body>
-        <table>
-            <thead>
-                <tr>
-                    <th></th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr></tr>
-            </tbody>
-        </table>
-    </body>
-    </html>
-    `);
-}).listen(8080);
+const server = http.createServer(async function(req, res){
+    res.statusCode = 200;
+    if(req.method === "GET"){
+        if(req.url=="/"){
+            res.writeHead(200,{"Content-Type": "text/html"});
+            const fileContent = fs.readFileSync("index.html");
+            res.end(fileContent, 'utf-8');
+        } else if(req.url == "/favicon.ico"){
+    
+        } else if(req.url == "/get-all-employees"){
+            const employees = await getAllEmployees();
+            res.writeHead(200,{'Content-Type': 'application/json'});
+            res.end(JSON.stringify(employees), 'utf-8');
+    
+        } else if(req.url.startsWith("/get-employee-detail/")){  // TODO
+            const id = parseInt(req.url.split("/")[2]); // Extract the ID from the URL
+            if (id > 0){
+                const employee = null;//await getAllEmployees();
+                res.writeHead(200,{'Content-Type': 'application/json'});
+                res.end(JSON.stringify(employee), 'utf-8');
+            }
+        } else if(req.url.startsWith("/get-employees-age-and-service")){
+            const parsedUrl = url.parse(req.url, true);
+            const query = parsedUrl.query;
+            const ids = query.ids.split(',').map(String);
+
+            const employeesAgeAndService = await getEmployeesAgeAndService(ids);
+            res.writeHead(200,{'Content-Type': 'application/json'});
+            res.end(JSON.stringify(employeesAgeAndService), 'utf-8');
+        } else {
+            res.writeHead(404, {'Content-Type': 'text/plain'});
+            res.end('Not found');
+        }
+        
+    
+    }
+    else if(req.method === "POST"){
+        if(req.url == "/get-employees-age"){
+            let body = "";
+            req.on("data", (chunk) => {
+                body += chunk.toString(); // Accumulate the request body data
+            });
+            req.on("end", async () => {
+                const request_data = JSON.parse(body);  // Parse the JSON data
+                res.writeHead(200,{'Content-Type': 'application/json'});
+                res.end(JSON.stringify(request_data), 'utf-8');
+            });
+        }
+    }
+});
+server.listen(process.env.PORT || 8080);
+
+async function getEmployeesAgeAndService(ids){
+    try {
+        var { status, stdout, stderr } = await callJava("get-employees-age-and-service", ids);
+        if(stdout) {
+            stdout = Buffer.from(stdout, 'base64').toString('utf-8');
+            return JSON.parse(stdout);
+        }
+        if(stderr){
+            console.log(stderr);
+        }
+    } catch (err) {
+        console.error("Nepričakovana napaka: " + err.message + "\n" + err.stack);
+    }
+    return [];
+}
 
 async function getAllEmployees(){
     try {
@@ -57,7 +98,8 @@ async function getAllEmployees(){
     return [];
 }
 
-async function callJava(api_endpoint) {
+async function callJava(api_endpoint, args) {
+    if (!args) { args = [] }
     const java = new JavaCaller({
 
         classPath: './',  // CLASSPATH as the path to the .class file or jar files if we write ./ it means that the JavaCallerTester.class file is in the same folder
@@ -69,7 +111,6 @@ async function callJava(api_endpoint) {
         minimumJavaVersion: 10
 
     });
-
-    const { status, stdout, stderr } = await java.run([api_endpoint]);
+    const { status, stdout, stderr } = await java.run([api_endpoint, ...args]);
     return { status, stdout, stderr };
 }
